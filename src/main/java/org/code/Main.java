1a9d84f94d9bc62916bb6c4b4b450834c8ec9cf8
bigdata72 <reviewking72@gmail.com>
2020-06-13 20:39:27 -0400

    initial commit

---
+++ src/main/java/org/code/Main.java
@@ -0,0 +1,15 @@
+package org.code;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		
+		LOGGER.info("hello there .. with maven build ");
+		LOGGER.debug("With debug");
+	}
+}
